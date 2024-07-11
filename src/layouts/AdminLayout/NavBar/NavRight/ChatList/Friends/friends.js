8300f4be1321102f3ad5948470a46dca10652507
Arunkumar <717821i106@kce.ac.in>
2024-07-11 11:35:55 +0530

    first commit

---
+++ src/layouts/AdminLayout/NavBar/NavRight/ChatList/Friends/friends.js
@@ -0,0 +1,194 @@
+const friends = [
+  {
+    id: 1,
+    photo: 'avatar-1.jpg',
+    name: 'Nithya',
+    new: 3,
+    status: 1,
+    time: 'typing'
+  },
+  {
+    id: 2,
+    photo: 'avatar-2.jpg',
+    name: 'Arun',
+    new: 1,
+    status: 1,
+    time: 'online'
+  },
+  {
+    id: 3,
+    photo: 'avatar-3.jpg',
+    name: 'Abdullah',
+    status: 1,
+    time: 'online'
+  },
+  {
+    id: 4,
+    photo: 'avatar-1.jpg',
+    name: 'Abhi',
+    status: 0,
+    new: 1,
+    time: '10 min ago'
+  },
+  {
+    id: 5,
+    photo: 'avatar-4.jpg',
+    name: 'Vetri',
+    status: 0,
+    time: '15 min ago'
+  },
+  {
+    id: 6,
+    photo: 'avatar-1.jpg',
+    name: 'Nandy',
+    new: 3,
+    status: 1,
+    time: 'typing'
+  },
+  {
+    id: 7,
+    photo: 'avatar-2.jpg',
+    name: 'Pary',
+    new: 1,
+    status: 1,
+    time: 'online'
+  },
+  {
+    id: 8,
+    photo: 'avatar-3.jpg',
+    name: 'Manoj',
+    status: 1,
+    time: 'online'
+  },
+  {
+    id: 9,
+    photo: 'avatar-1.jpg',
+    name: 'Alia',
+    status: 0,
+    new: 1,
+    time: '10 min ago'
+  },
+  {
+    id: 10,
+    photo: 'avatar-4.jpg',
+    name: 'Mithies',
+    status: 0,
+    time: '15 min ago'
+  },
+  {
+    id: 11,
+    photo: 'avatar-1.jpg',
+    name: 'Kruthika',
+    new: 3,
+    status: 1,
+    time: 'typing'
+  },
+  {
+    id: 12,
+    photo: 'avatar-2.jpg',
+    name: 'Abishek',
+    new: 1,
+    status: 1,
+    time: 'online'
+  },
+  {
+    id: 13,
+    photo: 'avatar-3.jpg',
+    name: 'Siva',
+    status: 1,
+    time: 'online'
+  },
+  {
+    id: 14,
+    photo: 'avatar-1.jpg',
+    name: 'Deepika',
+    status: 0,
+    new: 1,
+    time: '10 min ago'
+  },
+  {
+    id: 15,
+    photo: 'avatar-4.jpg',
+    name: 'Mukil',
+    status: 0,
+    time: '15 min ago'
+  },
+  {
+    id: 16,
+    photo: 'avatar-1.jpg',
+    name: 'Durga',
+    new: 3,
+    status: 1,
+    time: 'typing'
+  },
+  {
+    id: 17,
+    photo: 'avatar-2.jpg',
+    name: 'Simon',
+    new: 1,
+    status: 1,
+    time: 'online'
+  },
+  {
+    id: 18,
+    photo: 'avatar-3.jpg',
+    name: 'Yeswanth',
+    status: 1,
+    time: 'online'
+  },
+  {
+    id: 19,
+    photo: 'avatar-1.jpg',
+    name: 'Abhi',
+    status: 0,
+    new: 1,
+    time: '10 min ago'
+  },
+  {
+    id: 20,
+    photo: 'avatar-4.jpg',
+    name: 'Kumar',
+    status: 0,
+    time: '15 min ago'
+  },
+  {
+    id: 21,
+    photo: 'avatar-1.jpg',
+    name: 'Jaya',
+    new: 3,
+    status: 1,
+    time: 'typing'
+  },
+  {
+    id: 22,
+    photo: 'avatar-2.jpg',
+    name: 'Lary',
+    new: 1,
+    status: 1,
+    time: 'online'
+  },
+  {
+    id: 23,
+    photo: 'avatar-3.jpg',
+    name: 'Arunkumar',
+    status: 1,
+    time: 'online'
+  },
+  {
+    id: 24,
+    photo: 'avatar-1.jpg',
+    name: 'Sri Nithya',
+    status: 0,
+    new: 1,
+    time: '10 min ago'
+  },
+  {
+    id: 25,
+    photo: 'avatar-4.jpg',
+    name: 'Arun',
+    status: 0,
+    time: '15 min ago'
+  }
+];
+
+export default friends;
---
+++ src/layouts/AdminLayout/Navigation/NavContent/NavCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+
+
+const NavCard = () => {
+  return (
+    <React.Fragment>
+      <div className="p-20">
+        <Card className="pro-card">
+          <Card.Body className="p-2 text-center">
+            
+          </Card.Body>
+        </Card>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default NavCard;
